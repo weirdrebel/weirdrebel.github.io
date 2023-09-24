c6277b2c2f8c7c8d0ebf23c602ba7db3245c4a56
Abdul Moiz <89686178+weirdrebel@users.noreply.github.com>
2023-09-24 11:26:59 +0500

    Update script.js

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -20,9 +20,11 @@
     var listItem = document.createElement('li'); // Create a new list item element
 
     if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
       listItem.textContent = 'Good Bye ' + names[i];
       listItem.classList.add('goodbye'); // Apply the "goodbye" class
     } else {
+      helloSpeaker.speak(names[i]);
       listItem.textContent = 'Hello ' + names[i];
       listItem.classList.add('hello'); // Apply the "hello" class
     }
