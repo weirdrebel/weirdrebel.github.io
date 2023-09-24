d8f5a13d06b5adc8787f2eb037b6a3469cbcab84
Abdul Moiz <89686178+weirdrebel@users.noreply.github.com>
2023-09-24 11:20:46 +0500

    Module 4 - Solution

---
+++ module4-solution/js/script.js
@@ -0,0 +1,32 @@
+(function () {
+  var names = [
+    'Yaakov',
+    'John',
+    'Jen',
+    'Jason',
+    'Paul',
+    'Frank',
+    'Larry',
+    'Paula',
+    'Laura',
+    'Jim',
+    'Rebel',
+  ];
+
+  var nameList = document.querySelector('.name-list'); // Get the ul element
+
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    var listItem = document.createElement('li'); // Create a new list item element
+
+    if (firstLetter === 'j') {
+      listItem.textContent = 'Good Bye ' + names[i];
+      listItem.classList.add('goodbye'); // Apply the "goodbye" class
+    } else {
+      listItem.textContent = 'Hello ' + names[i];
+      listItem.classList.add('hello'); // Apply the "hello" class
+    }
+
+    nameList.appendChild(listItem); // Add the list item to the ul element
+  }
+})();
